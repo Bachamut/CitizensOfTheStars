37d140898e3f5941d6256402142d61e9f800e12f
Bachamut <lewand.luk@gmail.com>
2021-02-25 21:47:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+# initialize of pygame
+pygame.init()
+
+# create a screen
+screen = pygame.display.set_mode((800, 600))
+
+# title and icon
+pygame.display.set_caption("CitizensOfTheStars")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # screen background color (RBG)
+    screen.fill((50, 80, 150))
+
+    pygame.display.update()
